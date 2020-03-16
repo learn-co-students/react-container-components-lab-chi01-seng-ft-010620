de026b12447d469a38b8b04e52c77cb1ada6c22d
Zach Weber <weberzt@gmail.com>
2020-03-16 13:57:01 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,41 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(movies => {
+          let reviewsArr = []
+          movies.results.map(movie => {
+            let movieObj = {
+              display_title: movie.display_title,
+              suggested_link_text: movie.link.suggested_link_text,
+              url: movie.link.url
+            }
+            reviewsArr.push(movieObj)
+          })
+          this.setState({
+            reviews: reviewsArr
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className='latest-movie-reviews'>
+                <h2>The Latest Review:</h2>
+                <MovieReviews review={this.state.reviews} />
+            </div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer
