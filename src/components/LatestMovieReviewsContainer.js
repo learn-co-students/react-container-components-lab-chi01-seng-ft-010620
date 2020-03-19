a5c3ba6f1e972d0abfd3257f3f19bb01622a7a20
Lindsay Mecher <lindsaymecher@gmail.com>
2020-03-19 12:03:28 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,41 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          reviews: data['results']
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render(){
+    return(
+      <div className="latest-movie-reviews">
+        <h1>Latest Movie Reviews</h1>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer;
+
+// <LatestMovieReviewsContainer />
+//     ✓ should have state
+//     1) should have a state property "reviews"
+//     2) should have top-level element with class "latest-movie-reviews"
+//     3) should fetch data from the New York Times API
+//     4) should render reviews after reviews state updated
+//
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,57 @@
 // Code MovieReviews Here
+import React from 'react';
+
+// byline
+// "Ken Jaworowski"
+// critics_pick
+// 1
+// date_updated
+// "2020-03-18 16:44:01"
+// display_title
+// "Human Nature"
+// headline
+// "‘Human Nature’ Review: We Can Now Alter Our DNA. But Should We?"
+//
+// link
+// {suggested_link_text: "Read the New York Times Revi…}
+// mpaa_rating
+// ""
+//
+// multimedia
+// {height: 140, src: "https://static01.nyt.com/images…}
+// opening_date
+// "2020-03-12"
+// publication_date
+// "2020-03-12"
+// summary_short
+// "Adam Bolt’s new documentary focuses on Crispr technology, which can edit genes, thus giving people the ability to change human, animal and plant life."
+
+
+const MovieReviews = (props) => {
+  console.log(props.reviews);
+  const renderReviews = () => {
+    return props.reviews.map(review => {
+      return (
+        <div className="review" key={review.id}>
+        <h3>Display Title: {review.display_title}</h3>
+        <p>Headline: {review.headline}</p>
+        <p>Critic's Pick: {review.critics_pick}</p>
+        <img src={review.multimedia.src === null ? "https://image.shutterstock.com/image-vector/online-cinema-art-movie-poster-260nw-1006115221.jpg" : review.multimedia.src} />
+        <p>Short Summary: {review.summary_short}</p>
+      </div>
+    )
+    })
+  }
+  return(
+    <div className="review-list">
+      {renderReviews()}
+    </div>
+  )
+};
+
+export default MovieReviews;
+
+//   <MovieReviews />
+//     ✓ should be a stateless functional component
+//     5) should have a top-level component with class "review-list"
+//     6) should render all the reviews
